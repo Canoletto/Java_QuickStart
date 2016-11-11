f039aa8e7f1e7379690361b1fdefb717cd0f2d43
Canoletto <coding_ggt@mail.ru>
2016-11-11 04:10:34 +0300

    Включили цикл For в игру "Угадай число" для генерации уровней

--- Guess_The_Number/src/ru/geekbrains/guess_the_number/Main.java
+++ Guess_The_Number/src/ru/geekbrains/guess_the_number/Main.java
@@ -7,20 +7,17 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        System.out.println("Ваша задача - угадать число.");
         for(int i = 10; i <= 30; i += 10){
-            System.out.println(i);
+            int number = (int)(Math.random() * i);
+            playLevel (i, number);
         }
-//        System.out.println("Ваша задача - угадать число.");
-//        System.out.println("Введите максимальное число диапазона:");
-//        int range = scanner.nextInt();
-//        int number = (int)(Math.random()*range);
-//        playLevel (range, number);
-//        scanner.close();
+        scanner.close();
     }
 
     private static void playLevel(int range, int number){
         while(true){
-            System.out.println("Угадайте число от 0 до" + range);
+            System.out.println("Угадайте число от 0 до " + range);
             int input_number = scanner.nextInt();
             if(input_number == number){
                 System.out.println("Вы угадали!");
